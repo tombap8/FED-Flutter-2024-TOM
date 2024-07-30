7eddfd2128d3e6faa311c74a9d1ea833509f6bbd
Tom B. Parker <tombap8@hotmail.com>
2024-07-30 10:58:14 +0900

    Update 04.클래스믹스인.dart

--- 02.다트연습/04.클래스믹스인.dart
+++ 02.다트연습/04.클래스믹스인.dart
@@ -18,6 +18,37 @@
  * class 클래스명 with 믹스인클래스,믹스인클래스,...{}
 ****************************************************/
 
+void main() {
+  // 테슬라 자동차 인스턴스 생성하기!
+  // new Tesla() 인스턴스 생성시 new키워드 생략가능!
+  Tesla tesla = Tesla("테슬라", "Model 3", 6350.0);
+
+  print('🚘전기자동차 회사이름: ${tesla.compName}');
+  print('🚗모델명: ${tesla.model}');
+  print('🚗가격: ${tesla.price}만원');
+  print('🚗주행거리: ${tesla.distance}km');
+  // Tesla클래스 자신에게 없는 정보(믹스인 클래스정보)
+  print('🚗엔진정보: ${tesla.power}마력');
+  print('🚗바퀴정보: ${tesla.wheelInfo}');
+  print('🚗라이트정보: ${tesla.luminosity}lux');
+
+  // mixin 키워드만으로 생성된 클래스는 인스턴스화가 되는가?
+  // Engine eng = Engine(); -> 인스턴스화 불가!
+  // mixin 키워드만드로 만든 클래스는 부품같은 느낌이다!
+
+  // 믹스인도 되고 인스턴스로 생성도 되려면
+  // mixin class 클래스명{} -> 이렇게 생성하면 된다!!!
+  Light light = Light();
+  print('개별인스턴스 광도:${light.luminosity}lux');
+
+  // 상속의 방법이 아닌
+  // [ 컴포지션 방법 ]으로 다른 클래스 사용하기
+  // HyunDai 클래스 생성시 k엔진 클래스를 내부에 생성함!
+  HyunDai hd = HyunDai(K_Engine());
+  print('현대차엔진 파워:${hd.k_engine.power}마력');
+  print('현대차엔진 주행거리:${hd.k_engine.distance}km');
+} /////////////// main //////////////////////
+
 // 전기자동차 회사인 테슬라의 부품을 조달하도록 믹스인한다!
 class Tesla with Engine, Wheel, Light {
   // 회사명
@@ -30,12 +61,11 @@ class Tesla with Engine, Wheel, Light {
   double distance = 480.0;
 
   // 생성자 - 입력받을 속성값 셋팅!
-  Tesla(this.compName,this.model,this.price);
+  Tesla(this.compName, this.model, this.price);
 } /////////// Tesla 클래스 ////////////
 
-
 // 믹스인 클래스 : 엔진파트
-mixin Engine{
+mixin Engine {
   // 엔진마력수
   int power = 5000;
 }
@@ -45,8 +75,9 @@ mixin Wheel {
   // 바퀴정보
   String wheelInfo = "광폭4륜구동바퀴";
 }
+
 // 믹스인 클래스 : 라이트파트
-mixin class Light { 
+mixin class Light {
   // -> class 키워드사용으로 인스턴스가능!
   // 광도
   double luminosity = 3000.0;
@@ -64,11 +95,10 @@ class K_Engine {
 }
 
 //  현대차 클래스
-class HyunDai{
-  // 컴포지션 방법 : 
+class HyunDai {
+  // 컴포지션 방법 :
   // 1. 내부에 외부 클래스 변수형선언함!
   K_Engine k_engine;
   // 2. 생성자에 초기화등록함!
   HyunDai(this.k_engine);
 }
-
